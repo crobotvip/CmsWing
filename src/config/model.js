ded796f1a4e21ba2709d7f0a66f128877f73a1fb
罗鹏 <luopen@123.com>
2018-12-13 23:30:37 +0800

    u

--- src/config/model.js
+++ src/config/model.js
@@ -18,7 +18,7 @@ module.exports = {
     user: 'root', // 用户名
     password: 'crobot', // 密码
     database: 'cmswing', // 数据库
-    host: '127.0.0.1', // host
+    host: '47.99.208.107', // host
     port: 3306, // 端口
     connectionLimit: 1, // 连接池的连接个数，默认为 1
     prefix: 'cmswing_', // 数据表前缀，如果一个数据库里有多个项目，那项目之间的数据表可以通过前缀来区分
